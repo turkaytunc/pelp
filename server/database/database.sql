fad8a980bf44a2c614913d807e0c80f988a407ad
turkaytunc <turkaytunc00@gmail.com>
2021-03-04 14:21:39 +0300

    add: constraint to price range column

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE pern_stack_yelp;
+
 CREATE TABLE IF NOT EXISTS restaurants (
 id SERIAL PRIMARY KEY,
 name VARCHAR(70),
@@ -7,3 +9,4 @@ price_range integer
 
 
 INSERT INTO restaurants(name,location,price_range) values ('pasa', 'edirne', 4);
+ALTER TABLE restaurants ADD CONSTRAINT price_range check(price_range >= 1 and price_range <=5);
