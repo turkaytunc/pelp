e0c1a86037b98fabcf2f70c2d9266cae6bc12275
turkaytunc <turkaytunc00@gmail.com>
2021-03-04 15:27:53 +0300

    add: test setup

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 
 const { PORT = 4000 } = process.env;
 
+app.get('/', (_, res) => {
+  res.json({ message: 'hello' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
