350f7945ba894fb44e4327ffffd6c9819f846f29
turkaytunc <turkaytunc00@gmail.com>
2021-03-05 15:38:46 +0300

    add: form element

--- frontend/src/components/add-restaurant/AddRestaurant.tsx
+++ frontend/src/components/add-restaurant/AddRestaurant.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const AddRestaurant = () => {
-  return <div>Add Restaurant</div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={(event: React.FormEvent<HTMLFormElement>) => handleSubmit(event)}>
+        <label htmlFor="restaurant-name">
+          <input type="text" name="restaurant-name" id="restaurant-name" value={} />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default AddRestaurant;
