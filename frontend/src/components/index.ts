d23120436adba701337e8dd9e128a60537e273cc
turkaytunc <turkaytunc00@gmail.com>
2021-03-05 15:00:43 +0300

    update: folder structure for better readibility;

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from './header/Header';
+export { default as AddRestaurant } from './add-restaurant/AddRestaurant';
+export { default as RestaurantList } from './restaurant-list/RestaurantList';
