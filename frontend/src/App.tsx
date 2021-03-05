e7a3a51348a5c4f57e5109d5c4b64a295143f892
turkaytunc <turkaytunc00@gmail.com>
2021-03-05 14:00:52 +0300

    update: packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './app.scss';
+import { FaStarHalfAlt } from 'react-icons/fa';
 
 function App(): JSX.Element {
   return (
     <div className="app">
       <main>Learn React</main>
+      <FaStarHalfAlt />
     </div>
   );
 }
