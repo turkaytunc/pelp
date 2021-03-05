1c8f8f6304ff54d34e98cb787c698b8fd592c909
turkaytunc <turkaytunc00@gmail.com>
2021-03-05 16:05:19 +0300

    add: creating restaurant function

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Header } from '../components/index';
+import { Header, AddRestaurant } from '../components/index';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <AddRestaurant />
     </div>
   );
 };
--- frontend/src/util/addRestaurant.ts
+++ frontend/src/util/addRestaurant.ts
@@ -5,9 +5,9 @@ export const addRestaurant = (
   fetch: Function,
   url: string = 'http://localhost:5000/api/v1/restaurants'
 ) => {
-  window.fetch(url, {
+  fetch(url, {
     method: 'POST',
     headers: { Accept: 'application/json', 'Content-Type': 'application/json;charset=UTF-8' },
-    body: JSON.stringify('hey'),
+    body: JSON.stringify({ name, location, price_range: priceRange }),
   });
 };
