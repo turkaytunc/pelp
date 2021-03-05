1f8d99997bbe3a8ac5c24f21ef33c422ea846a0b
turkaytunc <turkaytunc00@gmail.com>
2021-03-05 14:40:20 +0300

    add: frontend routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import './app.scss';
 import { FaStarHalfAlt } from 'react-icons/fa';
+import { Home, RestaurantDetails, UpdateRestaurant } from './routes/index';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
-    <div className="app">
-      <main>Learn React</main>
-      <FaStarHalfAlt />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/restaurant/:id/update" component={UpdateRestaurant}></Route>
+          <Route exact path="/restaurant/:id" component={RestaurantDetails}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
