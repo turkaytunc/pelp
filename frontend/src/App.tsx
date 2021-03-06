f0c48d6a03883d2f8714869d3f03ef0901e500af
turkaytunc <turkaytunc00@gmail.com>
2021-03-06 19:43:48 +0300

    update: line endings config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './app.scss';
 // import { FaStarHalfAlt } from 'react-icons/fa';
 import { Home, RestaurantDetails, UpdateRestaurant } from './routes/index';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <div className="app">
         <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/restaurant/:id/update" component={UpdateRestaurant}></Route>
-          <Route exact path="/restaurant/:id" component={RestaurantDetails}></Route>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurant/:id/update" component={UpdateRestaurant} />
+          <Route exact path="/restaurant/:id" component={RestaurantDetails} />
         </Switch>
       </div>
     </BrowserRouter>
