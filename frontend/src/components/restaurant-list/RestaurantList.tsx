c7211c65778dac3f5c8cf189b41c052d209f366f
turkaytunc <turkaytunc00@gmail.com>
2021-03-09 19:36:24 +0300

    fix: lint errors

--- frontend/src/components/restaurant-list/RestaurantList.tsx
+++ frontend/src/components/restaurant-list/RestaurantList.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const RestaurantList = () => <div>Restaurant List</div>;
+const RestaurantList = (): React.ReactElement => (
+  <div>
+    <li>list item 1</li>
+    <li>list item 2</li>
+    <li>list item 3</li>
+  </div>
+);
 
 export default RestaurantList;
