5368b57cabb761abe9adb6978d9ae0b67fcb9f67
turkaytunc <turkaytunc00@gmail.com>
2021-03-10 16:10:20 +0300

    add: restaurant card renderer

--- frontend/src/components/restaurant-list/RestaurantList.tsx
+++ frontend/src/components/restaurant-list/RestaurantList.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './restaurant-list.scss';
 
-import { API_URL } from '../../constants';
+import { API_URL, Restaurant } from '../../type-helpers';
 import { getRestaurants } from '../../util';
 import ListCard from './list-card/ListCard';
 
 const RestaurantList = (): React.ReactElement => {
-  const [restaurants, setRestaurants] = useState([]);
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
 
   useEffect(() => {
     getRestaurants(`${API_URL}/restaurants`, window.fetch)
@@ -14,7 +14,8 @@ const RestaurantList = (): React.ReactElement => {
       .then((data) => setRestaurants(data));
   }, []);
 
-  const renderRestaurantList = () => restaurants?.map((restaurant) => <ListCard />);
+  const renderRestaurantList = () => restaurants?.map((restaurant) => <ListCard restaurant={restaurant} />);
+
   return (
     <div className="restaurant-list">
       {restaurants && renderRestaurantList()}
--- frontend/src/components/restaurant-list/list-card/ListCard.tsx
+++ frontend/src/components/restaurant-list/list-card/ListCard.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Restaurant } from '../../../type-helpers';
 
-const ListCard = (): React.ReactElement => (
-  <div className="listcard-container">
-    <img src="" alt="" />
-    <div className="listcard-details">
-      <div className="details-item">Name</div>
-      <div className="details-item">Location</div>
-      <div className="details-item">Price Range</div>
-      <div className="details-item">Ratings</div>
-      <button type="button" className="edit-button">
-        Edit
-      </button>
-      <button type="button" className="delete-button">
-        Delete
-      </button>
+const ListCard = ({ restaurant }: { restaurant: Restaurant }): React.ReactElement => {
+  const { id, name, location, priceRange } = restaurant;
+  return (
+    <div key={id} className="listcard-container">
+      <img src="" alt="" />
+      <div className="listcard-details">
+        <div className="details-item">{name}</div>
+        <div className="details-item">{location}</div>
+        <div className="details-item">{priceRange}</div>
+        <div className="details-item">Ratings</div>
+        <button type="button" className="edit-button">
+          Edit
+        </button>
+        <button type="button" className="delete-button">
+          Delete
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ListCard;
