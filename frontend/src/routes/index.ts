ae56210575d1412668fec08e19f9dcbcd8b13ee0
turkaytunc <turkaytunc00@gmail.com>
2021-03-11 16:33:40 +0300

    update: router export method

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,3 +1,3 @@
-export { default as Home } from './Home';
+export { default as Home } from './home/Home';
 export { default as RestaurantDetails } from './RestaurantDetails';
 export { default as UpdateRestaurant } from './UpdateRestaurant';
