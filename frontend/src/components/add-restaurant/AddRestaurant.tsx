95cd4033c96710c7cb622e8cd1fd8236524e8cd1
turkaytunc <turkaytunc00@gmail.com>
2021-03-11 11:54:45 +0300

    update: mobile layout

--- frontend/src/components/add-restaurant/AddRestaurant.tsx
+++ frontend/src/components/add-restaurant/AddRestaurant.tsx
@@ -19,36 +19,30 @@ const AddRestaurant = (): React.ReactElement => {
   return (
     <div>
       <form className="add-restaurant-form" onSubmit={(event: React.FormEvent<HTMLFormElement>) => handleSubmit(event)}>
-        <label className="add-restaurant-label" htmlFor="restaurant-name">
-          <p>Restaurant Name</p>
-          <input
-            type="text"
-            name="restaurant-name"
-            id="restaurant-name"
-            value={restaurantName}
-            onChange={(event) => setRestaurantName(event.target.value)}
-          />
-        </label>
-        <label className="add-restaurant-label" htmlFor="restaurant-location">
-          <p>Restaurant Location</p>
-          <input
-            type="text"
-            name="restaurant-location"
-            id="restaurant-location"
-            value={restaurantLocation}
-            onChange={(event) => setRestaurantLocation(event.target.value)}
-          />
-        </label>
-        <label className="add-restaurant-label" htmlFor="restaurant-price">
-          <p>Restaurant Price</p>
-          <input
-            type="text"
-            name="restaurant-price"
-            id="restaurant-price"
-            value={restaurantPrice}
-            onChange={(event) => setRestaurantPrice(event.target.value)}
-          />
-        </label>
+        <input
+          type="text"
+          name="restaurant-name"
+          id="restaurant-name"
+          value={restaurantName}
+          onChange={(event) => setRestaurantName(event.target.value)}
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          name="restaurant-location"
+          id="restaurant-location"
+          value={restaurantLocation}
+          onChange={(event) => setRestaurantLocation(event.target.value)}
+          placeholder="Location"
+        />
+        <input
+          type="text"
+          name="restaurant-price"
+          id="restaurant-price"
+          value={restaurantPrice}
+          onChange={(event) => setRestaurantPrice(event.target.value)}
+          placeholder="Price"
+        />
         <button type="submit">Add Restaurant</button>
       </form>
     </div>
