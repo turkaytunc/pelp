d5cb3fdfd3ffc661802bcd929bd92d4a9636b163
turkaytunc <turkaytunc00@gmail.com>
2021-03-11 12:33:55 +0300

    update: input fields validation

--- frontend/src/components/add-restaurant/AddRestaurant.tsx
+++ frontend/src/components/add-restaurant/AddRestaurant.tsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
-import { addRestaurant } from 'src/util';
+import { addRestaurant, convertFirstLetterToUpperCase, isInputEmpty } from 'src/util';
 import './add-restaurant.scss';
 
 const AddRestaurant = (): React.ReactElement => {
   const [restaurantName, setRestaurantName] = useState('');
   const [restaurantLocation, setRestaurantLocation] = useState('');
   const [restaurantPrice, setRestaurantPrice] = useState('');
+  const [inputError, setInputError] = useState('');
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (restaurantPrice === '' || restaurantLocation === '' || restaurantName === '') return;
-    addRestaurant(restaurantName, restaurantLocation, parseInt(restaurantPrice, 10), window.fetch);
+
+    if (isInputEmpty(restaurantName, restaurantPrice, restaurantLocation)) {
+      setInputError("Please don't left input fields blank!");
+      return;
+    }
+
+    addRestaurant(
+      convertFirstLetterToUpperCase(restaurantName),
+      convertFirstLetterToUpperCase(restaurantLocation),
+      parseInt(restaurantPrice, 10),
+      window.fetch
+    );
+
     setRestaurantPrice('');
     setRestaurantName('');
     setRestaurantLocation('');
@@ -25,6 +37,7 @@ const AddRestaurant = (): React.ReactElement => {
           id="restaurant-name"
           value={restaurantName}
           onChange={(event) => setRestaurantName(event.target.value)}
+          onFocus={() => setInputError('')}
           placeholder="Name"
         />
         <input
@@ -33,6 +46,7 @@ const AddRestaurant = (): React.ReactElement => {
           id="restaurant-location"
           value={restaurantLocation}
           onChange={(event) => setRestaurantLocation(event.target.value)}
+          onFocus={() => setInputError('')}
           placeholder="Location"
         />
         <input
@@ -41,8 +55,10 @@ const AddRestaurant = (): React.ReactElement => {
           id="restaurant-price"
           value={restaurantPrice}
           onChange={(event) => setRestaurantPrice(event.target.value)}
+          onFocus={() => setInputError('')}
           placeholder="Price"
         />
+        {inputError}
         <button type="submit">Add Restaurant</button>
       </form>
     </div>
