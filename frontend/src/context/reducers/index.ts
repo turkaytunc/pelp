31becb22ba598f6366bac3c8e68cb233c4c5b3ea
turkaytunc <turkaytunc00@gmail.com>
2021-03-11 22:07:39 +0300

    add: global state reducers

---
+++ frontend/src/context/reducers/index.ts
@@ -0,0 +1,1 @@
+export { restaurantReducer } from './restaurantReducer';
---
+++ frontend/src/context/reducers/restaurantReducer.ts
@@ -0,0 +1,21 @@
+import { ActionType } from 'src/constants';
+import { Restaurant } from '../../interfaces';
+import { RestaurantAction } from '../actions';
+
+export const restaurantReducer = (state: Restaurant[], action: RestaurantAction): Restaurant[] => {
+  switch (action.type) {
+    case ActionType.ADD_RESTAURANT: {
+      const { id, location, name, priceRange } = action.payload;
+      return [...state, { id, name, location, priceRange }];
+    }
+    case ActionType.REMOVE_RESTAURANT: {
+      return [...state.filter((e) => e.id !== action.payload)];
+    }
+    case ActionType.FETCH_RESTAURANTS: {
+      const restaurants = action.payload;
+      return [...state, ...restaurants];
+    }
+    default:
+      return state;
+  }
+};
