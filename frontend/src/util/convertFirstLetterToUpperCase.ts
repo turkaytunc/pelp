492bb72aaa0106b91d047890baf270e86f9d4c26
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-14 16:51:43 +0300

    add: file exclude configs to jest configurations

--- frontend/src/util/convertFirstLetterToUpperCase.ts
+++ frontend/src/util/convertFirstLetterToUpperCase.ts
@@ -1,4 +1,5 @@
 export const convertFirstLetterToUpperCase = (word: string): string => {
+  if (word.length < 2) return word.toUpperCase();
   const firstLetter = word[0];
   const newWord = `${firstLetter.toUpperCase()}${word.trim().slice(1)}`;
   return newWord;
