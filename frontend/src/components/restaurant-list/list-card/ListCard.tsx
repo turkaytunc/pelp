914fff4cdd6fca882cb35a2f13a2d38a25fa730c
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-14 20:18:35 +0300

    fix: restaurant details cannot be reached

--- frontend/src/components/restaurant-list/list-card/ListCard.tsx
+++ frontend/src/components/restaurant-list/list-card/ListCard.tsx
@@ -34,20 +34,23 @@ const ListCard = ({ restaurant }: { restaurant: Restaurant }): React.ReactElemen
   };
 
   return (
-    <div
-      onClick={() => handleDetails(id)}
-      role="button"
-      tabIndex={0}
-      onKeyDown={(event) => {
-        if (event.key === 'Enter') return handleDetails(id);
-        return null;
-      }}
-      className="listcard-container"
-    >
-      <img
-        src="https://camo.githubusercontent.com/d9b5643af3d25bd3d6842a691030fc4898d6a80a83ae6c6ef6d5e4f6de6ea8fb/68747470733a2f2f69636f6e67722e616d2f64657669636f6e2f6a6176617363726970742d6f726967696e616c2e7376673f73697a653d363026636f6c6f723d63757272656e74436f6c6f72"
-        alt="restaurant placeholder"
-      />
+    <div className="listcard-container">
+      <div
+        className="listcard-image-container"
+        role="button"
+        onClick={() => handleDetails(id)}
+        tabIndex={0}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') return handleDetails(id);
+          return null;
+        }}
+      >
+        <img
+          src="https://camo.githubusercontent.com/d9b5643af3d25bd3d6842a691030fc4898d6a80a83ae6c6ef6d5e4f6de6ea8fb/68747470733a2f2f69636f6e67722e616d2f64657669636f6e2f6a6176617363726970742d6f726967696e616c2e7376673f73697a653d363026636f6c6f723d63757272656e74436f6c6f72"
+          alt="restaurant placeholder"
+        />
+      </div>
+
       <div className="listcard-details">
         <div className="details-item"> {name}</div>
         <div className="details-item"> {location}</div>
