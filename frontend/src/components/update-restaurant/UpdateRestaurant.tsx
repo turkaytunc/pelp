2fa1146eabf1d5e07313da242462f22ca081324b
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-14 14:13:12 +0300

    add: preloaded input fields for update restaurants

--- frontend/src/components/update-restaurant/UpdateRestaurant.tsx
+++ frontend/src/components/update-restaurant/UpdateRestaurant.tsx
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { API_URL } from 'src/constants';
+import { Store } from 'src/context/Store';
 import { isInputEmpty, updateRestaurantById } from 'src/util';
 import './update-restaurant.scss';
 
 const UpdateRestaurant = (): React.ReactElement => {
   const { id }: { id: string } = useParams();
+  const history = useHistory();
+  const { state, dispatch } = useContext(Store);
 
   const [name, setName] = useState('');
   const [location, setLocation] = useState('');
   const [price, setPrice] = useState(1);
+
+  const setInitialInputs = () => {
+    const restaurant = state.restaurants.filter((item) => item.id === parseInt(id, 10));
+    if (restaurant[0]) {
+      setName(restaurant[0].name);
+      setLocation(restaurant[0].location);
+      setPrice(restaurant[0].priceRange);
+    }
+  };
+
+  useEffect(() => {
+    setInitialInputs();
+  }, []);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (isInputEmpty(name, location)) return;
     updateRestaurantById(id, API_URL, name, location, price);
+    history.push('/');
   };
 
   return (
