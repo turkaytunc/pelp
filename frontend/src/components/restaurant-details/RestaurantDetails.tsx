104dbcabab1e9206e0620436e7cfe32776c45e3e
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-17 16:11:04 +0300

    update: starRating component

--- frontend/src/components/restaurant-details/RestaurantDetails.tsx
+++ frontend/src/components/restaurant-details/RestaurantDetails.tsx
@@ -5,7 +5,7 @@ import './restaurant-details.scss';
 const RestaurantDetails = (): React.ReactElement => {
   return (
     <div className="restaurant-details-container">
-      <StarRate rating={5} />
+      <StarRate rating={1} />
     </div>
   );
 };
--- frontend/src/components/star-rate/StarRate.tsx
+++ frontend/src/components/star-rate/StarRate.tsx
@@ -5,11 +5,11 @@ import { FaStar, FaStarHalf, FaRegStar } from 'react-icons/fa';
 const StarRate = ({ rating }: { rating: number }): React.ReactElement => {
   const stars = [];
 
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 1; i <= 5; i += 1) {
     if (i <= rating) {
-      stars.push(<FaStar />);
+      stars.push(<FaStar key={`${i}`} />);
     } else {
-      stars.push(<FaRegStar />);
+      stars.push(<FaRegStar key={`${i}`} />);
     }
   }
 
