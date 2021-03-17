58d26f43796b8b8e90c5d736276db21de3a028c1
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-17 14:55:59 +0300

    add: StarRate component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from './header/Header';
 export { default as AddRestaurant } from './add-restaurant/AddRestaurant';
 export { default as RestaurantList } from './restaurant-list/RestaurantList';
 export { default as UpdateRestaurant } from './update-restaurant/UpdateRestaurant';
+export { default as StarRate } from './star-rate/StarRate';
---
+++ frontend/src/components/star-rate/StarRate.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './star-rate.scss';
+
+const StarRate = (): React.ReactElement => {
+  return <div>StarRate</div>;
+};
+
+export default StarRate;
