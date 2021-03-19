353aaf6a149a7e3c8db5d9d3e00db4798408fff2
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-19 15:50:55 +0300

    udpate: restaurant details grid layout

--- frontend/src/components/restaurant-details/RestaurantDetails.tsx
+++ frontend/src/components/restaurant-details/RestaurantDetails.tsx
@@ -5,8 +5,15 @@ import { ReviewCard, StarRate } from 'src/components';
 const RestaurantDetails = (): React.ReactElement => {
   return (
     <div className="restaurant-details-container">
-      <StarRate rating={1.2} />
-      <ReviewCard rating={3} />
+      <header>Restaurant Details</header>
+      <StarRate rating={3.3} />
+      <div className="restaurant-details-card-grid">
+        <ReviewCard rating={5} />
+        <ReviewCard rating={4} />
+        <ReviewCard rating={2} />
+        <ReviewCard rating={3} />
+        <ReviewCard rating={5} />
+      </div>
     </div>
   );
 };
