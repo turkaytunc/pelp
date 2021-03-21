c6e3f79981f6423709bd3b990297d1275308d983
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-21 20:01:29 +0300

    update: restaurant details page, now it fetches reviews from backend

--- frontend/src/components/restaurant-details/RestaurantDetails.tsx
+++ frontend/src/components/restaurant-details/RestaurantDetails.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './restaurant-details.scss';
 import { AddReview, ReviewCard, StarRate } from 'src/components';
+import { getReviewsByRestaurantId } from 'src/util';
+import { useParams } from 'react-router-dom';
+import { Review } from 'src/interfaces';
 
 const RestaurantDetails = (): React.ReactElement => {
+  const { id }: { id: string } = useParams();
+  const [restaurant, setRestaurant] = useState<Review[]>([]);
+  const [fetchError, setFetchError] = useState('');
+
+  useEffect(() => {
+    const fetchRestaurantReview = async () => {
+      try {
+        const response = await getReviewsByRestaurantId(id);
+
+        const data = (await response.json()) as Review[];
+        setRestaurant(data);
+      } catch (error) {
+        setFetchError(error.message);
+      }
+    };
+    fetchRestaurantReview();
+  }, []);
+
   return (
     <div className="restaurant-details-container">
       <header className="restaurant-details-header">Restaurant Details</header>
       <StarRate rating={3.3} />
       <div className="restaurant-details-card-grid">
-        <ReviewCard rating={5} />
-        <ReviewCard rating={4} />
-        <ReviewCard rating={2} />
-        <ReviewCard rating={3} />
-        <ReviewCard rating={5} />
+        {restaurant.map((item) => (
+          <ReviewCard rating={item.rating} name={item.name} comment={item.comment} />
+        ))}
       </div>
+      {fetchError}
       <AddReview />
     </div>
   );
--- frontend/src/util/getReviewsByRestaurantId.ts
+++ frontend/src/util/getReviewsByRestaurantId.ts
@@ -1,4 +1,4 @@
 import { API_URL } from 'src/constants/index';
 
-export const getReviewsByRestaurantId = async (id: number): Promise<Response> =>
+export const getReviewsByRestaurantId = async (id: string): Promise<Response> =>
   window.fetch(`${API_URL}/reviews/${id}`);
