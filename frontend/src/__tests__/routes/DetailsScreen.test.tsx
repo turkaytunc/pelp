6d6db72f63a4b32e558e8ab3c6dafcdc58d78efd
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-21 18:35:44 +0300

    fix: tests not working because undefined history api params

--- frontend/src/__tests__/routes/DetailsScreen.test.tsx
+++ frontend/src/__tests__/routes/DetailsScreen.test.tsx
@@ -1,8 +1,16 @@
 import { render } from '@testing-library/react';
 import { DetailsScreen } from 'src/routes';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 
 describe('<DetailsScreen />', () => {
   it('should render without crash', () => {
-    render(<DetailsScreen />);
+    const history = createBrowserHistory();
+    history.push('/restaurant/21');
+    render(
+      <Router history={history}>
+        <DetailsScreen />
+      </Router>
+    );
   });
 });
