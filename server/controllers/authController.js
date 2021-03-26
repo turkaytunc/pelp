3e768b493692d2fa69a35b66aa0329a9b57c2b2e
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-26 21:57:14 +0300

    add: check is user already exist

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,23 @@
 import pool from '../db/index.js';
 
 // POST /api/v1/register/
-export const createUser = (req, res, next) => {
+export const createUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
-    res.status(200).json({ name, email, password });
+    const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+    const isUserExist = user.rows.length > 0;
+    if (isUserExist) {
+      return res.status(401).send();
+    }
+
+    const createdUser = await pool.query(
+      'INSERT INTO users (name, password, email) VALUES ($1, $2, $3) RETURNING name, password',
+      [name, password, email]
+    );
+
+    return res.status(200).json({ user: createdUser.rows[0] });
   } catch (error) {
     error.statusCode = 400;
     return next(error);
