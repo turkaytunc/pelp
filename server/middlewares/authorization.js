e93ef2acbbd571ba706fabcf3e57884c95251397
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-28 13:36:14 +0300

    add: authorization middleware to verify jwt

---
+++ server/middlewares/authorization.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const secret = process.env.JWT_SECRET;
+
+export const authorization = (req, res, next) => {
+  try {
+    const authHeader = req.header('Authorization');
+    if (!authHeader) {
+      return res.status(403).send();
+    }
+    const verifyJwt = jwt.verify(authHeader, secret);
+    console.log(verifyJwt);
+
+    req.user = verifyJwt.user;
+    return next();
+  } catch (error) {
+    error.status = 403;
+    return next(error);
+  }
+};
