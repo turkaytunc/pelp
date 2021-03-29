3cb4be63dc576c98fb17df0666f959409f9456ed
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-29 19:51:22 +0300

    add: token to request header

--- frontend/src/util/addRestaurant.ts
+++ frontend/src/util/addRestaurant.ts
@@ -1,9 +1,16 @@
 import { API_URL } from 'src/constants';
 
-const addRestaurant = (name: string, location: string, priceRange: number): Promise<Response> =>
-  fetch(`${API_URL}/restaurants`, {
+const addRestaurant = (name: string, location: string, priceRange: number): Promise<Response> => {
+  const token = window.localStorage.getItem('token');
+
+  return fetch(`${API_URL}/restaurants`, {
     method: 'POST',
-    headers: { Accept: 'application/json', 'Content-Type': 'application/json;charset=UTF-8' },
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8',
+      Authorization: `Bearer ${token}`,
+    },
     body: JSON.stringify({ name, location, price_range: priceRange }),
   });
+};
 export default addRestaurant;
