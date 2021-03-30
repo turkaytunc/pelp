3737dd0a75833c0a3f8c6bc29f68f026ec5beb10
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-30 09:35:00 +0300

    fix: register route, jwt not created properly

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -2,9 +2,8 @@ import pool from '../db/index.js';
 
 export const getProfile = async (req, res, next) => {
   try {
-    console.log(req.user);
     const user = await pool.query('SELECT name, email FROM users WHERE user_unique = $1', [req.user]);
-    res.json({ ...user.rows[0] });
+    return res.json({ ...user.rows[0] });
   } catch (error) {
     next(error);
   }
--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -7,11 +7,13 @@ const secret = process.env.JWT_SECRET;
 export const authorization = (req, res, next) => {
   try {
     const authHeader = req.header('Authorization');
+
     if (!authHeader) {
       return res.status(403).send();
     }
     const token = authHeader.split(' ')[1];
     const verifyJwt = jwt.verify(token, secret);
+
     req.user = verifyJwt.user;
     return next();
   } catch (error) {
