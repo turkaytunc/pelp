885245d638e1d83d7474c7328a323033b424d5d1
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-31 18:51:07 +0300

    add: footer and navigation bar component files

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -10,3 +10,4 @@ export { default as Login } from './login/Login';
 export { default as Register } from './register/Register';
 export { default as Dashboard } from './dashboard/Dashboard';
 export { default as Loading } from './loading/Loading';
+export { default as NavigationBar } from './navigation-bar/NavigationBar';
---
+++ frontend/src/components/navigation-bar/NavigationBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './navigation-bar.scss';
+
+const NavigationBar = (): React.ReactElement => {
+  return <nav className="navigation-bar-container">Nav</nav>;
+};
+
+export default NavigationBar;
