22429b7a78fa85358a3005a0e91ec54d1dbaa767
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-31 19:24:06 +0300

    add: footer and navbar to main display area

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './app.scss';
+import { Footer, NavigationBar } from './components';
 import { HomeScreen, DetailsScreen, UpdateScreen, RegisterScreen, LoginScreen, DashboardScreen } from './routes';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter basename="/pern-stack-yelp">
       <div className="app">
-        <Switch>
-          <Route exact path="/" component={HomeScreen} />
-          <Route exact path="/dashboard" component={DashboardScreen} />
-          <Route exact path="/restaurant/:id/update" component={UpdateScreen} />
-          <Route exact path="/restaurant/:id" component={DetailsScreen} />
-          <Route exact path="/auth/register" component={RegisterScreen} />
-          <Route exact path="/auth/login" component={LoginScreen} />
-        </Switch>
+        <NavigationBar />
+        <div className="main">
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route exact path="/dashboard" component={DashboardScreen} />
+            <Route exact path="/restaurant/:id/update" component={UpdateScreen} />
+            <Route exact path="/restaurant/:id" component={DetailsScreen} />
+            <Route exact path="/auth/register" component={RegisterScreen} />
+            <Route exact path="/auth/login" component={LoginScreen} />
+          </Switch>
+        </div>
+        <Footer />
       </div>
     </BrowserRouter>
   );
