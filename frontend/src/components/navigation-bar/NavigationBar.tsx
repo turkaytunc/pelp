5cb355e1b8527f2ba0586a099ac968c0145a4cfb
Türkay Tunç <turkaytunc00@gmail.com>
2021-03-31 20:06:49 +0300

    add: navbar

--- frontend/src/components/navigation-bar/NavigationBar.tsx
+++ frontend/src/components/navigation-bar/NavigationBar.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FaYelp } from 'react-icons/fa';
+import { Link, useHistory } from 'react-router-dom';
+import { Store } from 'src/context/Store';
 import './navigation-bar.scss';
 
 const NavigationBar = (): React.ReactElement => {
+  const { state, dispatch } = useContext(Store);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('token');
+    window.location.pathname = '/';
+  };
+
   return (
     <div className="navigation-bar-container">
-      <section>
-        <img src="#" alt="company logo with ocean" />
+      <section className="nav-section-logo">
+        <Link to="/">
+          <FaYelp size={30} fill="rgb(200, 40, 90)" />
+        </Link>
       </section>
-      <section>
-        <ul>
-          <li>Dashboard</li>
-          <li>Sign in</li>
-          <li>Sign up </li>
-        </ul>
+      <section className="nav-section-links">
+        {state.user.isAuth ? (
+          <>
+            <Link to="/dashboard">Dashboard</Link>
+            <button onClick={handleLogout} type="button">
+              Logout
+            </button>
+          </>
+        ) : (
+          <Link to="/auth/login">Sign in</Link>
+        )}
       </section>
     </div>
   );
