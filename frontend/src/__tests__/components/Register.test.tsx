7bf0dbed3fd3840bed9ac4b374eada6d0b9ffbc7
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-01 18:36:49 +0300

    add: api call tests for register component

--- frontend/src/__tests__/components/Register.test.tsx
+++ frontend/src/__tests__/components/Register.test.tsx
@@ -2,6 +2,9 @@ import { screen, fireEvent, render } from '@testing-library/react';
 import { Register } from 'src/components';
 import { StoreProvider } from 'src/context/Store';
 
+jest.spyOn(window, 'fetch');
+afterEach(jest.clearAllMocks);
+
 describe('<Register />', () => {
   it('should render without crashing', async () => {
     render(
@@ -47,7 +50,7 @@ describe('<Register />', () => {
     });
   });
 
-  describe('submit events', () => {
+  describe('failed submit events', () => {
     it('should handle empty submit ops', async () => {
       render(
         <StoreProvider>
@@ -67,12 +70,89 @@ describe('<Register />', () => {
             <Register />
           </StoreProvider>
         );
+
         fireEvent.submit(await screen.findByText('Sign Up'));
         expect(await screen.findByText("Please don't left inputs empty!")).toBeInTheDocument();
 
         fireEvent.focus(await screen.findByTestId('register-name'));
         fireEvent.focus(await screen.findByTestId('register-email'));
         fireEvent.focus(await screen.findByTestId('register-password'));
+
+        expect(await screen.findByTestId('register-name')).toHaveValue('');
+        expect(await screen.findByTestId('register-email')).toHaveValue('');
+        expect(await screen.findByTestId('register-password')).toHaveValue('');
+      });
+    });
+  });
+
+  describe('successful submit event', () => {
+    describe('successful register', () => {
+      it('should register user and save jwt to localstorage', async () => {
+        (window.fetch as jest.Mock).mockResolvedValue({
+          status: 200,
+          json: jest.fn(() => ({ token: 'fsdfjksfd', user: { name: 'username', email: 'usermail@example.com' } })),
+        });
+
+        render(
+          <StoreProvider>
+            <Register />
+          </StoreProvider>
+        );
+
+        fireEvent.change(await screen.findByTestId('register-name'), { target: { value: 'hasan' } });
+        fireEvent.change(await screen.findByTestId('register-email'), { target: { value: 'hasangeyik@gmail.com' } });
+        fireEvent.change(await screen.findByTestId('register-password'), { target: { value: 'thisisasecurepass123' } });
+        fireEvent.submit(await screen.findByText('Sign Up'));
+
+        expect(await screen.findByTestId('register-name')).toHaveValue('');
+        expect(await screen.findByTestId('register-email')).toHaveValue('');
+        expect(await screen.findByTestId('register-password')).toHaveValue('');
+      });
+    });
+    describe('failed register', () => {
+      it('should fail to register user', async () => {
+        (window.fetch as jest.Mock).mockResolvedValue({
+          status: 400,
+          json: jest.fn(() => ({ message: 'Error Occured' })),
+        });
+
+        render(
+          <StoreProvider>
+            <Register />
+          </StoreProvider>
+        );
+
+        fireEvent.change(await screen.findByTestId('register-name'), { target: { value: 'hasan' } });
+        fireEvent.change(await screen.findByTestId('register-email'), { target: { value: 'hasangeyik@gmail.com' } });
+        fireEvent.change(await screen.findByTestId('register-password'), {
+          target: { value: 'thisisasecurepass123' },
+        });
+        fireEvent.submit(await screen.findByText('Sign Up'));
+
+        expect(await screen.findByText('Error Occured')).toBeTruthy();
+
+        expect(await screen.findByTestId('register-name')).toHaveValue('');
+        expect(await screen.findByTestId('register-email')).toHaveValue('');
+        expect(await screen.findByTestId('register-password')).toHaveValue('');
+      });
+
+      it('should get fetch error', async () => {
+        (window.fetch as jest.Mock).mockRejectedValue(new Error('Cannot Fetch'));
+
+        render(
+          <StoreProvider>
+            <Register />
+          </StoreProvider>
+        );
+
+        fireEvent.change(await screen.findByTestId('register-name'), { target: { value: 'hasan' } });
+        fireEvent.change(await screen.findByTestId('register-email'), { target: { value: 'hasangeyik@gmail.com' } });
+        fireEvent.change(await screen.findByTestId('register-password'), {
+          target: { value: 'thisisasecurepass123' },
+        });
+        fireEvent.submit(await screen.findByText('Sign Up'));
+
+        expect(await screen.findByText('Cannot Fetch')).toBeTruthy();
       });
     });
   });
