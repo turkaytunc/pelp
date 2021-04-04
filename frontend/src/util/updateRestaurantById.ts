6f1bed82b0c4e2a152a8436cd15fc3d1b97fe5b6
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-04 10:01:49 +0300

    add: authorization header for protected route fetch requests

--- frontend/src/util/updateRestaurantById.ts
+++ frontend/src/util/updateRestaurantById.ts
@@ -1,11 +1,14 @@
 import { API_URL } from 'src/constants';
 
-export const updateRestaurantById = (id: string, name: string, location: string, price: number): Promise<Response> =>
-  window.fetch(`${API_URL}/restaurants/${id}`, {
+export const updateRestaurantById = (id: string, name: string, location: string, price: number): Promise<Response> => {
+  const token = window.localStorage.getItem('token');
+  return fetch(`${API_URL}/restaurants/${id}`, {
     method: 'PUT',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json;charset=UTF-8',
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ name, location, price_range: price }),
   });
+};
