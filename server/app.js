1b60a549209eba730e72b10718de97a3972cacc6
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-04 09:54:29 +0300

    refactor: server.js file, now it only contains express listen method

---
+++ server/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import { authRoutes, dashboardRoutes, restaurantRoutes, reviewRoutes } from './routers/v1/index.js';
+import { ErrorWithStatusCode } from './util/index.js';
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// routes
+app.use('/api/v1/restaurants', restaurantRoutes);
+app.use('/api/v1/reviews', reviewRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/dashboard', dashboardRoutes);
+
+// Unhandled Endpoint Error
+app.get('/*', (req, res, next) => {
+  const error = new ErrorWithStatusCode('Page Not Found');
+  error.status = 404;
+  return next(error);
+});
+
+// Global Error Handler
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.status || 500);
+  return res.json({ message: error.message || 'An unexpected error occurred!' });
+});
+
+export default app;
