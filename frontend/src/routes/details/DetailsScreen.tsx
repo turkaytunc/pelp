3cc45999fe0cdec2ca4c4af693660d889f5ad181
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-04 21:13:23 +0300

    update: screen containers for desktop layout

--- frontend/src/routes/details/DetailsScreen.tsx
+++ frontend/src/routes/details/DetailsScreen.tsx
@@ -3,9 +3,9 @@ import { RestaurantDetails } from 'src/components/';
 import './details-screen.scss';
 
 const DetailsScreen = (): React.ReactElement => (
-  <>
+  <div className="details-screen-container">
     <RestaurantDetails />
-  </>
+  </div>
 );
 
 export default DetailsScreen;
