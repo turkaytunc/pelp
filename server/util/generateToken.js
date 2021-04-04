5acdef13db377e78850b8e0821fc3ffaa3a47600
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-04 13:03:29 +0300

    add: eslint setup

--- server/util/generateToken.js
+++ server/util/generateToken.js
@@ -5,7 +5,9 @@ dotenv.config();
 const secret = process.env.JWT_SECRET;
 const TEN_MIN = 1000 * 60 * 10;
 
-export const generateToken = (userId) => {
+const generateToken = (userId) => {
   const token = jwt.sign({ user: userId }, secret, { expiresIn: `${TEN_MIN}ms` });
   return token;
 };
+
+export default generateToken;
--- server/util/index.js
+++ server/util/index.js
@@ -1,3 +1,3 @@
-export { generateToken } from './generateToken.js';
+export { default as generateToken } from './generateToken.js';
 export { default as ErrorWithStatusCode } from './ErrorWithStatusCode.js';
 export * as joiValidators from './joiValidators.js';
